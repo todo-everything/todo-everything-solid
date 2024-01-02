db480eba541be89d7d131ffb9b6f0646f8de5ad6
Allan Almazan <allan.almazan@gmail.com>
2024-01-02 13:56:04 -0800

    Linting, prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,11 @@
+/* eslint-env node */
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json'
+  },
+  plugins: ['@typescript-eslint'],
+
+  root: true,
+};
